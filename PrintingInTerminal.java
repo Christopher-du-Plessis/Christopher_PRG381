b9b4d3e45bad7e5858007841767a7df207d78182
Christopher-du-Plessis <cjm.dup@gmail.com>
2021-02-24 22:15:52 +0200

    Improved the message in develop branch

--- PrintingInTerminal.java
+++ PrintingInTerminal.java
@@ -2,6 +2,7 @@ public class PrintingInTerminal{
     public static void main(String[] args){
         System.out.println("Printing message in terminal");
         System.out.println("Making a change");
+        System.out.println("Developing something in the develop branch");
         //Adding comment 1
     }
 }
