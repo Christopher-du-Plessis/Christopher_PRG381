a123dc4d2a2647049f60976011727e354c9a2654
Christopher-du-Plessis <cjm.dup@gmail.com>
2021-02-24 22:06:56 +0200

    Made a change to the message and added a comment

--- PrintingInTerminal.java
+++ PrintingInTerminal.java
@@ -1,5 +1,7 @@
 public class PrintingInTerminal{
     public static void main(String[] args){
         System.out.println("Printing message in terminal");
+        System.out.println("Making a change");
+        //Adding comment 1
     }
 }
